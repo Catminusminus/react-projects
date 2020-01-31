3a5791944b249c8a80aea55c24536e41c358d173
Catminusminus <resonafolma@gmail.com>
2020-02-01 00:25:37 +0900

    add error handling

--- github-portfolio/src/components/List/index.tsx
+++ github-portfolio/src/components/List/index.tsx
@@ -16,20 +16,29 @@ const Label = styled.span`
   font-weight: strong;
 `
 
+const Title = styled.h2`
+  padding: 10px 0;
+  border-bottom: 1px solid lightGrey;
+`
+
 interface Props {
   items: any[]
+  title: string
   children?: never
 }
 
-const List: React.FC<Props> = ({ items }) => (
-  <ListWrapper>
-    {items.map(item => (
-      <ListItem key={item.label}>
-        <Label>{item.label}</Label>
-        {item.value}
-      </ListItem>
-    ))}
-  </ListWrapper>
+const List: React.FC<Props> = ({ title, items }) => (
+  <>
+    <Title>{title}</Title>
+    <ListWrapper>
+      {items.map(item => (
+        <ListItem key={item.label}>
+          <Label>{item.label}</Label>
+          {item.value}
+        </ListItem>
+      ))}
+    </ListWrapper>
+  </>
 )
 
 export default List
--- github-portfolio/src/containers/Profile.tsx
+++ github-portfolio/src/containers/Profile.tsx
@@ -16,20 +16,31 @@ const Profile = () => {
   const ref = React.useRef(null)
   const [data, setData] = React.useState({} as any)
   const [loading, setLoading] = React.useState(true)
+  const [repositories, setRepositories] = React.useState([] as any[])
+  const [error, setError] = React.useState('')
   React.useEffect(() => {
     const fetchProfile = async () => {
       const profile = await fetch('https://api.github.com/users/octocat')
       const profileJSON = await profile.json()
       if (profileJSON) {
+        const repositoriesFetched = await fetch(profileJSON.repos_url)
+        const repositoriesJSON = await repositoriesFetched.json()
+
         setData(profileJSON)
         setLoading(false)
+        setRepositories(repositoriesJSON)
       }
     }
-    fetchProfile()
+    try {
+      fetchProfile()
+    } catch (error) {
+      setLoading(false)
+      setError(error.message)
+    }
   }, [ref])
 
-  if (loading) {
-    return <div>Loading...</div>
+  if (loading || error) {
+    return <div>{loading ? 'Loading...' : error}</div>
   }
 
   const items = [
@@ -45,10 +56,16 @@ const Profile = () => {
     { label: 'bio', value: data.bio },
   ]
 
+  const projects = repositories.map(repository => ({
+    label: repository.name,
+    value: <Link url={repository.html_url} title="GitHub  URL" />,
+  }))
+
   return (
     <ProfileWrapper>
       <Avatar src={data.avatar_url} alt="avatar" />
-      <List items={items} />
+      <List title="Profile" items={items} />
+      <List title="Projects" items={projects} />
     </ProfileWrapper>
   )
 }
